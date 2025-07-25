1ffdb3867082a791705bf5cc99ecee81108a2234
Baburao Adkane <b.g.adkane27@gmail.com>
2025-07-25 23:01:43 +0530

    About section modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,71 @@
+// import { useGSAP } from "@gsap/react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
 import { Brain, CalendarCheck, Lightbulb, MessageSquare, RefreshCw, SearchCheck, Smile, Target, UserRound, Users } from "lucide-react";
+import { useLayoutEffect, useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const About = () => {
+  const containerRef = useRef(null);
+  const nameRef = useRef(null);
+  const quoteRef = useRef(null);
+  const taglineRef = useRef(null);
+
+  useGSAP(() => {
+    const nameSplit = new SplitText(nameRef.current, { type: "chars" });
+    const quoteSplit = new SplitText(quoteRef.current, { type: "chars" });
+    const taglineSplit = new SplitText(taglineRef.current, { type: "chars" });
+
+    gsap.from(nameSplit.chars, {
+      scrollTrigger: {
+        trigger: nameRef.current,
+        start: "top 80%",
+        end: "top 20%",
+        scrub: true,
+        toggleActions: "play reverse play reverse",
+        markers:true
+      },
+      opacity: 0,
+      y: 50,
+      stagger: 0.05,
+      duration: 1,
+      ease: "expo.out",
+    });
+
+    gsap.from(quoteSplit.chars, {
+      scrollTrigger: {
+        trigger: quoteRef.current,
+        start: "top 90%",
+        end: "top 60%",
+        scrub: true,
+        toggleActions: "play reverse play reverse",
+      },
+      opacity: 0,
+      y: 40,
+      stagger: 0.04,
+      duration: 1,
+      ease: "power4.out",
+    });
+
+    gsap.from(taglineSplit.chars, {
+      scrollTrigger: {
+        trigger: taglineRef.current,
+        start: "top 90%",
+        end: "top 60%",
+        scrub: true,
+        toggleActions: "play reverse play reverse",
+      },
+      opacity: 0,
+      y: 30,
+      stagger: 0.03,
+      duration: 1,
+      ease: "power4.out",
+    });
+  }, []);
+
   return (
     <section id="about" className="relative overflow-hidden">
       {/* Background Grid and Glows */}
@@ -28,15 +93,31 @@ const About = () => {
       <div className="absolute inset-0 bg-gradient-to-b from-black/60 to-violet-100/10 pointer-events-none" />
 
       {/* Hero Section */}
-      <div className="relative z-10 text-center w-full min-h-screen max-w-7xl mx-auto flex flex-col justify-between items-center px-6 py-24 sm:px-8 md:px-16">
-        <p className="text-xs tracking-widest uppercase">About Me</p>
+      <div
+        ref={containerRef}
+        className="relative z-10 text-center w-full min-h-screen max-w-7xl mx-auto flex flex-col justify-between items-center px-6 py-24 sm:px-8 md:px-16"
+      >
+        <p className="text-xs tracking-widest uppercase text-blue-400">About Me</p>
 
         <div className="flex flex-col">
-          <h1 className="text-4xl md:text-5xl font-bold tracking-wide mb-4">Mr. Baburao Adkane</h1>
-          <p className="capitalize text-lg md:text-xl font-medium">" Quality isn't a task — it's a discipline "</p>
+          <h1
+            ref={nameRef}
+            className="text-6xl md:text-9xl font-bold tracking-wide mb-4"
+          >
+            Baburao Adkane
+          </h1>
+          <p
+            ref={quoteRef}
+            className="capitalize text-lg md:text-xl font-medium"
+          >
+            " Quality isn't a task — it's a discipline "
+          </p>
         </div>
 
-        <p className="mt-6 text-sm tracking-wider capitalize">
+        <p
+          ref={taglineRef}
+          className="mt-6 text-sm tracking-wider capitalize text-gray-300"
+        >
           “ Committed to delivering quality and reliability in every project ”
         </p>
       </div>
@@ -45,7 +126,7 @@ const About = () => {
       <div className="relative z-10 max-w-7xl mx-auto px-6 py-16 sm:px-8 md:px-16">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-12">
           <div className="bg-black/30 border p-6 rounded-tr-4xl">
-            <UserRound className="mb-3 text-[#8C39D9]" size={28}/>
+            <UserRound className="mb-3 text-[#8C39D9]" size={28} />
             <h2 className="text-lg font-semibold mb-2">Who I Am</h2>
             <p className="text-sm leading-relaxed">
               I have a deep understanding of testing strategies, bug tracking, and delivering quality software products.
